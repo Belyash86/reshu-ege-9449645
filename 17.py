a1bdebccec61e5d3b99785b5343d19520cbe936d
Belyash86 <92972932+Belyash86@users.noreply.github.com>
2021-10-22 12:20:34 +0500

    -

--- 17.py
+++ 17.py
@@ -1,7 +1,6 @@
 f = open('17.txt')
 f = [int(f.readline()) for _ in range(10000)]
-k = 0
-k_max = 0
+k = k_max = 0
 for i in range(9999):
     for j in range(i+1, 10000):
         if (abs(f[i]-f[j]) % 45 == 0) and (f[i] % 18 == 0 or f[j] % 18 == 0):
